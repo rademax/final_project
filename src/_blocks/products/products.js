6c1165cbc01fffc51a11e9ce581ad2836ef17331
Vladimir Ishchenko <rademaxbh@gmail.com>
2018-06-14 12:11:12 +0300

    Edit products from JS (release without products__list in html)

--- src/_blocks/products/products.js
+++ src/_blocks/products/products.js
@@ -1,39 +1,22 @@
-let products = document.getElementsByClassName('products')[1];
-//
-//   constructor (prop) {
-//     this.id = prop.id;
-//     this.dateAdded = prop.dateAdded;
-//     this.title = prop.title;
-//     this.description = prop.description;
-//     this.placeholder = prop.placeholder;
-//     this.discountedPrice = prop.discountedPrice;
-//     this.price = prop.price;
-//     this.hasNew = prop.hasNew;
-//     this.category = prop.category;
-//     this.fashion = prop.fashion;
-//     this.colors = prop.colors;
-//     this.sizes = prop.sizes;
-//     this.thumbnail = prop.thumbnail;
-//     this.preview = prop.preview;
-//   }
+let productsHtml = document.getElementsByClassName('products')[0];
+let productsCountGlobal = performProductsCount();
 
 function formProductItem(product) {
-  let templateCard = `
+  return `
     <div class="products__item">
-        <div class="products__img">
-            <img src="img/new_products/product1.jpg" alt="${product.title}">
-            <div class="products__hover"><a href="item.html" class="products__hover-link">View item</a></div>
-        </div>
-        ${hasNew(product.hasNew)}
-        <div class="products__item-title">
-            <a class="products__link" href="item.html">${product.title}</a>
-        </div>
-        <div class="products__price">
-            ${hasDiscount(product.price, product.discountedPrice)}
-        </div>
-    </div>
-    `;
-  return templateCard;
+      <div class="products__img">
+        <img src="img/new_products/product1.jpg" alt="${product.title}">
+        <div class="products__hover"><a href="item.html" class="products__hover-link">View item</a></div>
+      </div>
+      ${hasNew(product.hasNew)}
+      <div class="products__item-title">
+        <a class="products__link" href="item.html">${product.title}</a>
+      </div>
+      <div class="products__price">
+        ${hasDiscount(product.price, product.discountedPrice)}
+      </div>
+  </div>
+  `;
 }
 
 function hasDiscount(price, discountedPrice) {
@@ -57,13 +40,67 @@ function hasNew(hasNew) {
 
 function addProductsOnPage(products = catalog) {
   let productsList = document.createElement("div");
+  let productsCountOnRow = performProductsCount();
   productsList.classList.add("products__list");
   for(let i = 0; i < products.length; i++) {
     let product = products[i];
+    if(i === productsCountOnRow) {
+      productsList.innerHTML += addPromoBanner();
+    }
     productsList.innerHTML += formProductItem(product);
   }
+  productsHtml.appendChild(productsList);
+  productsHtml.innerHTML += addShowMoreButton();
+}
 
-  products.appendChild(productsList);
+function addShowMoreButton() {
+  return `<a class="btn products__all-products" href="#">Show more</a>`;
+}
+
+function performProductsCount() {
+  let clientWidth = checkClientWidth();
+  let productsCountOnRow = 4;
+  if(clientWidth >= 768 &&  clientWidth < 1024) {
+    productsCountOnRow = 3;
+  }
+  else if(clientWidth < 768) {
+    productsCountOnRow = 2;
+  }
+  return productsCountOnRow;
+}
+
+function checkClientWidth() {
+  return window.innerWidth;
+}
+
+function addPromoBanner() {
+  return `
+    <section class="promo wrap">
+      <div class="promo__title">Last weekend <span>extra 50%</span> off on all reduced boots and shoulder bags</div>
+      <div class="promo__description">This offer is valid in-store and online. Prices displayed reflect this additional discount. This offer ends at 11:59 GMT on March 1st 2015</div>
+    </section>
+  `;
 }
 
 addProductsOnPage();
+
+window.addEventListener("resize", function () {
+  let addBanner = checkProductsCountChanges();
+  if(addBanner) {
+    clearProductsFromPage();
+    addProductsOnPage();
+  }
+});
+
+function clearProductsFromPage() {
+  productsHtml.innerHTML = '';
+}
+
+function checkProductsCountChanges() {
+  let productsCount = performProductsCount();
+  if(productsCount !== productsCountGlobal) {
+    productsCountGlobal = productsCount;
+    return true;
+  }
+  return false;
+}
