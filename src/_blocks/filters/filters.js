fe71fca6a4e495b656a97a5b3f9996072cdefc1c
rademax <rademaxbh@gmail.com>
2018-06-16 15:51:41 +0300

    Update filters

--- src/_blocks/filters/filters.js
+++ src/_blocks/filters/filters.js
@@ -14,27 +14,41 @@ function changeFilterItem(target) {
   let filterItem = target;
   let filter = filterItem.parentNode;
   let filterName = filter.parentNode.getElementsByClassName('filter__name')[0];
+  let filterItems = filter.getElementsByTagName('li');
 
-  if(filterItem.classList.contains('active')) {
-    removeActiveFilter(filterItem);
+  if(filterItem.innerHTML === 'Not selected' && filterItem.classList.contains('active')) {
+    return 0;
+  }
+
+  deactivateFilterItems(filterItems);
+  activateFilterItem(filterItem);
+
+  if(filterItem.innerHTML === 'Not selected') {
+    deleteBlockAboutActiveFilter(filterItem);
     toggleActiveItem(filterName);
     return 0;
   }
 
   if(filterName.classList.contains('active')) {
-    changeActiveFilter(filterItem);
+    changeBlockAboutActiveFilter(filterItem);
   } else {
-    addActiveFilter(filterName, filterItem);
+    addBlockAboutActiveFilter(filterItem, filterName);
+    toggleActiveItem(filterName);
   }
+}
+
+function activateFilterItem(filterItem) {
   filterItem.classList.add('active');
 }
 
-function changeActiveFilter(filterItem) {
-  let filter = filterItem.parentNode;
-  let filterItemContent = filterItem.innerHTML;
-  let selectedFilter =  filter.parentNode.getElementsByClassName('filter__selected')[0];
-  selectedFilter.innerHTML = filterItemContent;
-  let filterItems = filter.getElementsByTagName('li');
+function addBlockAboutActiveFilter(filterItem, filterName) {
+  let filterSelected = document.createElement('div');
+  filterSelected.className = 'filter__selected';
+  filterSelected.innerHTML = filterItem.innerHTML;
+  filterName.insertAdjacentElement('afterend', filterSelected);
+}
+
+function deactivateFilterItems(filterItems) {
   for(let i = 0; i < filterItems.length; i++) {
     if (filterItems[i].classList.contains('active')) {
       filterItems[i].classList.remove('active');
@@ -42,20 +56,22 @@ function changeActiveFilter(filterItem) {
   }
 }
 
-function addActiveFilter(filterName, filterItem) {
-  let filterItemContent = filterItem.innerHTML;
-  toggleActiveItem(filterName);
-  let filterSelected = document.createElement('div');
-  filterSelected.className = 'filter__selected';
-  filterSelected.innerHTML = filterItemContent;
-  filterName.insertAdjacentElement('afterend', filterSelected);
+function changeBlockAboutActiveFilter(filterItem) {
+  let selectedFilter = defineSelectedFilter(filterItem);
+  selectedFilter.innerHTML = filterItem.innerHTML;
 }
 
-function removeActiveFilter(filterItem) {
+function defineSelectedFilter(filterItem) {
   let filter = filterItem.parentNode;
-  filterItem.classList.remove('active');
-  let filterSelected = filter.parentNode.getElementsByClassName('filter__selected')[0];
-  filter.parentNode.removeChild(filterSelected);
+  return filter.parentNode.getElementsByClassName('filter__selected')[0];
+}
+
+function deleteBlockAboutActiveFilter(filterItem) {
+  let filter = filterItem.parentNode;
+  let selectedFilter = defineSelectedFilter(filterItem);
+  if(filter.parentNode.contains(selectedFilter)) {
+    filter.parentNode.removeChild(selectedFilter);
+  }
 }
 
 function toggleActiveItem(item) {
